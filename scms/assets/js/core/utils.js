bb8f040e8ed291b7a736081b68b35eb53b54d645
Wyze <linhrathhenry414@gmail.com>
2025-12-09 14:23:22 +0700

    first commit

---
+++ scms/assets/js/core/utils.js
@@ -0,0 +1,7 @@
+// Helper functions
+export const $ = (selector) => document.querySelector(selector);
+export const $$ = (selector) => document.querySelectorAll(selector);
+
+export function getPageName() {
+    return document.body.getAttribute('data-page-name') || '';
+}
